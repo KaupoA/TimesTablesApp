c902a4281d0985cc6adcca06c56f8ae35681e8f8
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-16 22:48:13 +0200

    Completed the app

--- app/src/main/java/com/kast/timestablesapp/MainActivity.java
+++ app/src/main/java/com/kast/timestablesapp/MainActivity.java
@@ -1,13 +1,84 @@
 package com.kast.timestablesapp;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView numbersList;
+
+    public void generateTimesTable(int timesTable) {
+
+        final ArrayList<String> arrayNumbers = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+
+            arrayNumbers.add(Integer.toString(i * timesTable));
+
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayNumbers);
+
+        numbersList.setAdapter(arrayAdapter);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        numbersList = (ListView)findViewById(R.id.numbersList);
+
+        // SeekBar
+
+        seekBar.setMax(20);
+        seekBar.setProgress(10);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int min = 1;
+                int timesTable;
+
+                if (progress < min) {
+
+                    timesTable = min;
+                    seekBar.setProgress(min);
+
+                } else {
+
+                    timesTable = progress;
+
+                }
+
+                generateTimesTable(timesTable);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        generateTimesTable(10);
+
     }
 }
